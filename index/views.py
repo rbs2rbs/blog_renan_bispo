6888428f4fe072598e991bf175c37e19cce7a9b1
rbs2rbs <renan.bisposilva@gmail.com>
2020-04-19 10:03:40 -0300

    Ajusta experiencia

--- index/views.py
+++ index/views.py
@@ -4,10 +4,11 @@ from index.models import Perfil, Educacao, Habilidade, Case
 def home(request):
 
     perfil = Perfil.objects.all()
+    experiencia = Experiencia.objects.all()
     educacao = Educacao.objects.all()
     principal = Habilidade.objects.filter(prioridade="principal")
     secundaria = Habilidade.objects.filter(prioridade="secundaria")
     case = Case.objects.all()
 
 
-    return render(request, 'index.html', {'perfil':perfil, 'educacao':educacao, 'principal':principal, 'secundaria':secundaria, 'case':case})
+    return render(request, 'index.html', {'perfil':perfil, 'experiencia':experiencia,'educacao':educacao, 'principal':principal, 'secundaria':secundaria, 'case':case})
