25ae86d3fbb18154c70f076a593b397a1afb829b
rbs2rbs <renan.bisposilva@gmail.com>
2020-04-19 13:56:45 -0300

    Ordena cases

--- index/views.py
+++ index/views.py
@@ -8,7 +8,7 @@ def home(request):
     educacao = Educacao.objects.all()
     principal = Habilidade.objects.filter(prioridade="principal")
     secundaria = Habilidade.objects.filter(prioridade="secundaria")
-    case = Case.objects.order_by("data_criado")
+    case = Case.objects.order_by("-data_criado")
 
 
     return render(request, 'index.html', {'perfil':perfil, 'experiencia':experiencia,'educacao':educacao, 'principal':principal, 'secundaria':secundaria, 'case':case})
